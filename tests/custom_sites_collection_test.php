ef571343145a198b1987e8788e57e377135e408e
Matt Friedman <maf675@gmail.com>
2026-09-05 16:50:43 -0700

    remove moddb.yml because dependable server-side resolution is no longer available
    Del

--- tests/custom_sites_collection_test.php
+++ tests/custom_sites_collection_test.php
@@ -50,7 +50,6 @@ class custom_sites_collection_test extends \phpbb_test_case
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/bilibili.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/dotsub.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/ebaumsworld.yml', $collection);
-		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/moddb.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/ok.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/schooltube.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/snotr.yml', $collection);
